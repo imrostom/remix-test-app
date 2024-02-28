36cd0886e22d91ac2256d9bb501e4123f7fa3041
Rostom <rostomali4444@gmail.com>
2024-02-28 10:17:05 +0600

    remix app

--- app/db.server.js
+++ app/db.server.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 mongoose.Promise = global.Promise;
 
+console.log(process.env.DATABASE_URL, 'dd');
+
 await mongoose.connect(process.env.DATABASE_URL)
     .then(() => {
         console.log('Connected!');
