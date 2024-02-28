3bc1d2ce8366193e92bc016c33310c177cf0599e
Rostom <rostomali4444@gmail.com>
2024-02-28 10:10:55 +0600

    fix

--- app/db.server.js
+++ app/db.server.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 mongoose.Promise = global.Promise;
 
-await mongoose.connect('mongodb://localhost:27017/remix-crud')
+await mongoose.connect(process.env.DATABASE_URL)
     .then(() => {
         console.log('Connected!');
     }).catch((err) => {
